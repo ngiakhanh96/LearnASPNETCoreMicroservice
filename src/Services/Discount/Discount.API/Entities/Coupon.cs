d0eb9d6ca1ba8e43495ca3bd429d4b0eb4e1a320
WhiteK <ngiakhanh96@gmail.com>
2021-06-23 23:03:09 +0700

    Refinished 51

---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Discount.API.Entities
+{
+    // Test with record
+    public record Coupon(Guid Id)
+    {
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
