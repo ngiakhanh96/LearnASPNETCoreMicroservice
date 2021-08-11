b81a121c53246580e33d16067c7bd4837f81acf7
Khanh Gia Nguyen <ngiakhanh96@gmail.com>
2021-08-11 23:03:35 +0700

    Refinished 118

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent() : this(Guid.NewGuid(), DateTime.UtcNow)
+        {
+            
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime createdDate)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+        }
+
+        public Guid Id { get; private set; }
+
+        public DateTime CreatedDate { get; private set; }
+    }
+}
